d89f5b91af02c11198ca8d9845395c907b0a4b7e
Ashish Kumar Shah <ashish.k.shah@oracle.com>
2017-08-26 15:24:31 +0530

    Adding buttons and speed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import Grid from './grid.js';
-let gridInput = [
+var gridInput = [
   [1, 1, 1, 1,1,1,1,1,1,1,1],
   [1, null, null,null, null, null, null, null, null, null, 1],
   [1, null, null,null, null, null, null, null, null, null, 1],
@@ -21,3 +21,12 @@ let gridInput = [
   [1, 1,1,1, 1,1,1,1,1,1,1]
 ];
 var g = new Grid(gridInput);
+var leftMovementHandler = g.setLeftAsNextDirection.bind(g);
+var rightMovementHandler = g.setRightAsNextDirection.bind(g);
+var upMovementHandler = g.setUpAsNextDirection.bind(g);
+var downMovementHandler = g.setDownAsNextDirection.bind(g);
+
+document.getElementById('left').addEventListener('click', leftMovementHandler);
+document.getElementById('right').addEventListener('click', rightMovementHandler);
+document.getElementById('up').addEventListener('click', upMovementHandler);
+document.getElementById('down').addEventListener('click', downMovementHandler);
